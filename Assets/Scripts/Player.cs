ba7fc17fcbe8e8c52caefda537ea3bda66255b86
Scott Bromander <scott@primeacademy.io>
2016-06-27 10:41:20 -0500

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	public Image starImage;
+	public Sprite iconStar;
+	public Sprite iconNoStar;
+	private bool carryingStar = false;
+
+	//From other file where we were practicing MVC
+	//private PlayerInventoryDisplay playerInventoryDisplay;
+
+
+	//Once again, used for the MVC structure, but not used here.
+	// Use this for initialization
+	//void Start () {
+		//playerInventoryDisplay = GetComponent<PlayerInventoryDisplay> ();
+		//playerInventoryDisplay.OnChangeCarryingStar (carryingStar);
+	//}
+
+	void OnTriggerEnter2D(Collider2D hit){
+		if(hit.CompareTag("Star")){
+			carryingStar = true;
+			//playerInventoryDisplay.OnChangeCarryingStar (carryingStar);
+			UpdateStarImage();
+			Destroy (hit.gameObject);
+		}
+	}
+	
+
+	void UpdateStarImage () {
+		if (carryingStar) {
+			starImage.sprite = iconStar;
+		} else {
+			starImage.sprite = iconNoStar;
+		}
+	}
+}
